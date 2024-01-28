30d98f6a2fb617f2a9f6053c4aa616707d7558fc
parkmiri <parkmiri0722@gmail.com>
2024-01-28 15:45:23 +0900

    add FileStructure

---
+++ src/main/java/server/drawwang/DrawWangApplication.java
@@ -0,0 +1,13 @@
+package server.drawwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawWangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawWangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/drawwang/DrawWangApplicationTests.java
@@ -0,0 +1,13 @@
+package server.drawwang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawWangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
