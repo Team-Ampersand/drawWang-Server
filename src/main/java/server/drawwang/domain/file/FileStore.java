a77e18bc7030e2f2a7f2c64ff9a0711d39b56fa3
이현준 <11dlguswns@naver.com>
2024-03-07 09:06:01 +0900

    디렉토리를 직접 생성하게 변경

--- src/main/java/server/drawwang/domain/file/FileStore.java
+++ src/main/java/server/drawwang/domain/file/FileStore.java
@@ -1,6 +1,5 @@
 package server.drawwang.domain.file;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import server.drawwang.global.exception.CustomErrorCode;
@@ -8,6 +7,7 @@ import server.drawwang.global.exception.CustomException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Component
@@ -18,7 +18,7 @@ public class FileStore {
             return null;
         }
 
-        return System.getProperty("user.dir") + File.separator + "/src/main/resources/static/" + fileName;
+        return Paths.get(getFilePath(), fileName).toString();
     }
 
     public String getPartialImagesPath(String fileName) {
@@ -26,7 +26,7 @@ public class FileStore {
             return "";
         }
 
-        return "/static/" + fileName;
+        return File.separator + Paths.get("static", "images", fileName);
     }
 
     public String storeFile(MultipartFile multipartFile)
@@ -34,15 +34,29 @@ public class FileStore {
         if (multipartFile.isEmpty()) {
             return null;
         }
+
+        makeDir();
+
         String originalFilename = multipartFile.getOriginalFilename();
         String storeFileName = createStoreFileName(originalFilename);
-
         try {
             multipartFile.transferTo(new File(getFullPath(storeFileName)));
         } catch (IOException e) {
             throw new CustomException(CustomErrorCode.FILE_PROCESSING_ERROR);
         }
+
         return storeFileName;
+    }
+
+    private String getFilePath() {
+        return Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "static", "images").toString();
+    }
+
+    private void makeDir() {
+        File file = new File(getFilePath());
+        if (!file.exists() && !file.mkdirs()) {
+            throw new CustomException(CustomErrorCode.FILE_CREATE_ERROR);
+        }
     }
 
     private String createStoreFileName(String originalFilename) {
